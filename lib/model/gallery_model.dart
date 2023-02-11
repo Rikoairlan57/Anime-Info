ed52f3bb51837404f0d829be577a9fc0c9f067e2
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-11 13:27:26 +0700

    ✅ ✨ fixed data gallery

--- lib/model/gallery_model.dart
+++ lib/model/gallery_model.dart
@@ -41,48 +41,28 @@ var imageList = [
     id: '8',
     imageGallery: 'images/gallery/kimetsu2.jpeg',
   ),
-  // ImageData(
-  //   id: '9',
-  //   imageGallery: 'https://picsum.photos/seed/image009/500/600',
-  // ),
-  // ImageData(
-  //   id: '10',
-  //   imageGallery: 'https://picsum.photos/seed/image010/500/900',
-  // ),
-  // ImageData(
-  //   id: '11',
-  //   imageGallery: 'https://picsum.photos/seed/image011/500/900',
-  // ),
-  // ImageData(
-  //   id: '12',
-  //   imageGallery: 'https://picsum.photos/seed/image012/500/700',
-  // ),
-  // ImageData(
-  //   id: '13',
-  //   imageGallery: 'https://picsum.photos/seed/image013/500/700',
-  // ),
-  // ImageData(
-  //   id: '14',
-  //   imageGallery: 'https://picsum.photos/seed/image014/500/800',
-  // ),
-  // ImageData(
-  //   id: '15',
-  //   imageGallery: 'https://picsum.photos/seed/image015/500/500',
-  // ),
-  // ImageData(
-  //   id: '16',
-  //   imageGallery: 'https://picsum.photos/seed/image016/500/700',
-  // ),
-  // ImageData(
-  //   id: '17',
-  //   imageGallery: 'https://picsum.photos/seed/image017/500/600',
-  // ),
-  // ImageData(
-  //   id: '18',
-  //   imageGallery: 'https://picsum.photos/seed/image018/500/900',
-  // ),
-  // ImageData(
-  //   id: '19',
-  //   imageGallery: 'https://picsum.photos/seed/image019/500/800',
-  // ),
+  ImageData(
+    id: '9',
+    imageGallery: 'images/gallery/ngnl1.jpeg',
+  ),
+  ImageData(
+    id: '10',
+    imageGallery: 'images/gallery/bc3.jpeg',
+  ),
+  ImageData(
+    id: '11',
+    imageGallery: 'images/gallery/ngnl2.jpeg',
+  ),
+  ImageData(
+    id: '12',
+    imageGallery: 'images/gallery/bc.jpeg',
+  ),
+  ImageData(
+    id: '13',
+    imageGallery: 'images/gallery/bc2.jpeg',
+  ),
+  ImageData(
+    id: '14',
+    imageGallery: 'images/gallery/gojo1.jpeg',
+  ),
 ];
