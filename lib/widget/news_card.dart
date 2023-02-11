a9a343e82c68c5f24a6dbeb4d92462de26a41112
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-02-11 22:14:12 +0700

    🎉 ✨ 🎨 created ui news info anime

---
+++ lib/widget/news_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:anime_info/model/news_model.dart';
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          final NewsAnime news = newsAnime[index];
+          return InkWell(
+            onTap: () {},
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(news.imageNews),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            news.title,
+                            style: const TextStyle(fontSize: 16.0),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: newsAnime.length,
+      ),
+    );
+  }
+}
